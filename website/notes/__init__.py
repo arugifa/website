b38d5c46bd05f969564861452cb2f5a6d8dbb124
Alexandre Figura <arugifa@users.noreply.github.com>
2019-01-12 17:27:48 +0100

    MVP: Remove life notes and recommended articles

--- website/notes/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-notes = Blueprint('notes', __name__, template_folder='templates')
-
-from . import views  # noqa: E402, F401
--- website/recommended/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-recommended = Blueprint('recommended', __name__, template_folder='templates')
-
-from . import views  # noqa: E402, F401
